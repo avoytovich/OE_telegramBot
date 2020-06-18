84b845f223f0f303836239c436e93eeb5232c1fb
avoytovich <levanwork@gmail.com>
2020-06-19 02:25:09 +0300

    fix: deploy v2

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
 const { secret } = require('./config.secret.js');
 
+const url = {
+  setWebHook: `https://api.telegram.org/bot${secret}/setwebhook`,
+  sendMessage: `https://api.telegram.org/bot${secret}/sendMessage`,
+};
+
 module.exports = {
-  url: {
-    setWebHook: `https://api.telegram.org/bot${secret}/setwebhook`,
-    sendMessage: `https://api.telegram.org/bot${secret}/sendMessage`,
-  },
+  url,
 };
