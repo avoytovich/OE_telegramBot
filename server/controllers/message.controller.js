0a5d0b7fad2e139d959a8527de13ae93da79b399
avoytovich <levanwork@gmail.com>
2020-06-18 13:09:20 +0300

    fix: deploy_v.01

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,10 +1,23 @@
+const axios = require('axios');
 const Reply = require('../helper/reply.message');
+const { secret } = require('../../config/config.secret');
 
 module.exports = {
   message(req, res, next) {
-    const { message } = req.body;
-    if (message) {
-      return Reply.universal(req, res, next);
+    const env = process.env.NODE_ENV ? 'production' : 'development';
+    if (env == 'production') {
+      axios
+        .post(`https://api.telegram.org/bot${secret}/setwebhook`, {
+          url: 'https://oe-telegram-bot.herokuapp.com/new-message',
+        })
+        .then(() => {
+          const { message } = req.body;
+          if (message) {
+            return Reply.universal(req, res, next);
+          }
+          res.end();
+        })
+        .catch((err) => console.log(err));
     }
   },
 };
